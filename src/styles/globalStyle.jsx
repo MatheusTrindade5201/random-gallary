9eef84ceceb7544d8bd7843c610f1ffdba15b533
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2022-12-25 19:41:20 -0300

    feat: defining a global style to the page using createGlobalStyle from style component

---
+++ src/styles/globalStyle.jsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        min-height: 100vh;
+        background-image: linear-gradient(#041833, #154580);
+        box-sizing: border-box;
+    }
+
+`
+
+export default GlobalStyle
