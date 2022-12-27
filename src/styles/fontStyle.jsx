1024f6ab7e157b9746e3dfc81a10cbf5286f7eef
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2022-12-27 20:36:19 -0300

    fix: adjusting the structure of font face

--- src/styles/fontStyle.jsx
+++ src/styles/fontStyle.jsx
@@ -4,7 +4,10 @@ import inter from '../assets/fonts/static/Inter-Regular.ttf'
 const FontStyle = createGlobalStyle`
     @font-face {
         font-family: 'Inter';
-        src: url(${inter});
+        src: local('Inter')
+        url(${inter}) format('truetype');
+        font-weight: 500;
+        font-display: swap
     }
 `
 
