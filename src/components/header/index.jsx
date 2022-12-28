f1df7181bfccd27aefcbd0f42d1e499dd5d5386a
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2022-12-28 20:37:27 -0300

    feat: adding socials in the header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,15 @@
 import { HeaderWrapper } from "./style"
 import logo from '../../assets/images/logo.svg'
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 
 const Header = () => {
     return(
         <HeaderWrapper>
             <img src={logo} />
+            <div className="social">
+                <a href="https://github.com/MatheusTrindade5201" target='_blank'><AiFillGithub /></a>
+                <a href="https://www.linkedin.com/in/matheus-trindade5201/" target='_blank'><AiFillLinkedin /></a>
+            </div>
         </HeaderWrapper>
     )
 }
--- src/components/header/style.js
+++ src/components/header/style.js
@@ -2,8 +2,27 @@ import styled from "styled-components";
 
 export const HeaderWrapper = styled.header`
     padding: 2rem 3vw;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
     img{
         width: 360px
+    }
+
+    .social {
+        display: flex;
+        gap: 1rem
+    }
+
+    a{
+        font-size: 3rem;
+        color: white;
+        transition: .2s
+    }
+
+    a:hover{
+        color: #B9C0FF;
+        cursor: pointer;
     }
 `
